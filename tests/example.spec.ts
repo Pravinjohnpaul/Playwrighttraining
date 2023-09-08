5a3eb11a1c7f61bbc876c31a292e3a45508faee4
Pravin John Paul <Pravin.Paul@ATMECS.CORP>
2023-09-08 18:37:40 +0530

    added

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,16 @@
-import { test, expect } from '@playwright/test';
+import { test, expect} from "@playwright/test";
+// describe ('launch browser', () => {
+  test ('Hello, world',async()=>{
+      const message= 'Hello, world';
+      console.log(message);
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  //   // const browser = await chromium.launch();
+  //   const context = await browser.newContext();
+  //   const page = await context.newPage();
+  //   await page.goto(`https://playwright.dev/doc`);
+  //   await context.close();
+  })
+// });
